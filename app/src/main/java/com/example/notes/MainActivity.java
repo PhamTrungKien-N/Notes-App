838ca1dc3d2693f7d02fec2db6f1ade91f4a8a78
PhamTrungKien-N <18520957@gm.uit.edu.vn>
2022-05-06 12:48:50 +0700

    the third

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.notes.Adapters.NotesListAdapter;
+import com.example.notes.Database.RoomDB;
+import com.example.notes.Models.Notes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    RoomDB database;
+    FloatingActionButton fab_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+        database= RoomDB.getInstance(this);
     }
 }
---
+++ app/src/main/java/com/example/notes/NotesClickListener.java
@@ -0,0 +1,14 @@
+package com.example.notes;
+
+import androidx.cardview.widget.CardView;
+
+import com.example.notes.Models.Notes;
+
+public class NotesClickListener {
+    public void onClick(Notes notes) {
+    }
+    public void onLongClick(Notes notes, CardView cardView){
+    }
+
+
+}

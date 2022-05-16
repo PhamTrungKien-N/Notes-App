92ff7ce8ad57f96192bbda7127125e5e6a146c7e
PhamTrungKien-N <18520957@gm.uit.edu.vn>
2022-05-17 03:03:28 +0700

    Update activity

---
+++ app/src/main/java/com/example/notesroomdb/view/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.notesroomdb.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notesroomdb.MyApplication
+import com.example.notesroomdb.R
+import com.example.notesroomdb.databinding.ActivityMainBinding
+import com.example.notesroomdb.viewmodel.NotesViewModel
+import com.example.notesroomdb.viewmodel.ViewModelFactory
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val noteViewModel: NotesViewModel by lazy{
+        ViewModelProvider(this, ViewModelFactory((application as MyApplication).repository)).get(NotesViewModel::class.java)
+    }
+
+    private var adapter:Adapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupUI()
+        title = "Notes"
+
+
+    }
+
+    private fun setupUI() {
+
+        //val rv = findViewById<RecyclerView>(R.id.rv)
+        //val addNote = findViewById<Button>(R.id.btnAddNote)
+
+        binding.btnAddNote.setOnClickListener {
+            val intent = Intent(this, NewNote::class.java)
+            startActivity(intent)
+        }
+
+        adapter = Adapter(emptyList())
+
+        binding.apply {
+        rv.adapter = adapter
+            //val layoutManager = GridLayoutManager(applicationContext,2)
+            val layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+        rv.layoutManager = layoutManager
+         }
+
+        noteViewModel.notesList.observe(this, {note ->
+            note?.let { list ->
+                adapter?.update(list)
+            }
+        }
+        )
+    }
+
+
+}
